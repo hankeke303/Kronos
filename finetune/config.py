552fe663bb456819696a22ef0704861ce501c607
hankeke303 <hankeke303@163.com>
2026-05-15 14:45:43 +0000

    chore: update finetune defaults

--- finetune/config.py
+++ finetune/config.py
@@ -22,7 +22,7 @@ class Config:
         # self.run_id = "test_20251126_0034_train_predictor_with_new_tokenizer"
         # self.run_id = "test_20251126_0918_train_predictor_with_new_tokenizer_scratch"
         # self.run_id = "test_202512011451_predictor_w_tokenizer11301359_l0072-2"
-        self.run_id = "test_20260326_new-12-features-14bit_new-predictor-seqlen120"
+        self.run_id = "zmj_test_20260429_new-12-features-14bit_new-predictor-seqlen120_predict_30"
 
         # Overall time range for data loading from Qlib.
         self.dataset_begin_time = "2005-01-04"
@@ -30,7 +30,7 @@ class Config:
 
         # Sliding window parameters for creating samples.
         self.lookback_window = 120  # Number of past time steps for input.
-        self.predict_window = 10  # Number of future time steps for prediction.
+        self.predict_window = 30  # Number of future time steps for prediction.
         self.max_context = 512  # Maximum context length for the model.
 
         # Features to be used from the raw data.
@@ -78,10 +78,10 @@ class Config:
         self.train_time_range = ["2005-01-04", "2022-12-31"]
         self.val_time_range = ["2022-09-01", "2024-06-30"]
         self.test_time_range = ["2024-04-01", "2025-11-07"]
-        self.backtest_time_range = ["2024-07-01", "2025-11-07"]
+        self.backtest_time_range = ["2024-07-01", "2025-11-06"]
 
         # TODO: Directory to save the processed, pickled datasets.
-        self.dataset_path = "~/workspace/kronos/20260409"
+        self.dataset_path = "./data/processed_datasets_110"
         # self.dataset_path = "./data/processed_datasets_cleaned_6"
 
         # =================================================================
@@ -233,13 +233,8 @@ class Config:
         self.inference_top_p = 0.9
         self.inference_top_k = 0
         self.inference_sample_count = 5
-        self.inference_use_kv_cache = True
         self.backtest_batch_size = 1000
         self.backtest_benchmark = self._set_benchmark(self.instrument)
-        # 是否允许在序列起始阶段使用“不满 lookback_window 的上下文”。
-        # False: 与原逻辑一致，必须凑满 lookback_window 才会产出该时点的信号。
-        # True: 允许从数据起点截取到当前锚点，尽早生成信号，减少开头缺口。
-        self.backtest_allow_partial_context = False
         
         # =================================================================
         # New added by ZMJ
@@ -247,12 +242,6 @@ class Config:
         self.backtest_pred = 'close_return'    # supported: 'close' or 'close_return' now, 20260112
         if self.backtest_pred == 'close_return':
             self.backtest_save_folder_name += '_closereturn'
-
-        # 当开启短上下文模式时，在结果目录名打标，便于区分实验。
-        if self.backtest_allow_partial_context:
-            self.backtest_save_folder_name += '_partialctx'
-            
-        self.backtest_save_folder_name += "_by_data_20260409" + "_test_kvcache"
 
     def _set_benchmark(self, instrument):
         dt_benchmark = {
